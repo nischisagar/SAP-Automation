31f08ee7ac502d001b0f0da89b7b4a96f44dc8f8
pogovind <pogovind@in.ibm.com>
2020-10-28 21:49:25 +0530

    removed the '

--- roles/get-the-sap-version/files/sap-version-check.sh
+++ roles/get-the-sap-version/files/sap-version-check.sh
@@ -3,5 +3,5 @@ sid=`db2 -x "select current server from sysibm.sysdummy1"`
 SCHEMA=`db2 -x "select tabschema from syscat.tables where tabname='CVERS_TXT'"`
 sapvers=`db2 -x "select distinct varchar(STEXT,25) from $SCHEMA.CVERS_TXT where LANGU='E'"`
 echo "$sid" > /tmp/sap-version-check.log
-echo "'$sapvers'" >> /tmp/sap-version-check.log
+echo "$sapvers" >> /tmp/sap-version-check.log
 disp+work|egrep -i 'variant|number' >> /tmp/sap-version-check.log
