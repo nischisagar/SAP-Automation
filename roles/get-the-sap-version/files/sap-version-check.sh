2babd9561f9e4df3792e462d0cb0e6260e25b49f
pogovind <pogovind@in.ibm.com>
2020-10-28 20:38:48 +0530

    removed tab space

--- roles/get-the-sap-version/files/sap-version-check.sh
+++ roles/get-the-sap-version/files/sap-version-check.sh
@@ -2,6 +2,6 @@
 sid=`db2 -x "select current server from sysibm.sysdummy1"`
 SCHEMA=`db2 -x "select tabschema from syscat.tables where tabname='CVERS_TXT'"`
 sapvers=`db2 -x "select distinct varchar(STEXT,25) from $SCHEMA.CVERS_TXT where LANGU='E'"`
-echo "SID         : $sid"
-echo "SAP Version : $sapvers"
-disp+work|egrep -i 'variant|number'
+echo "$sid" > /tmp/sapvers.log
+echo "$sapvers" >> /tmp/sapvers.log
+disp+work|egrep -i 'variant|number' >> /tmp/sapvers.log
