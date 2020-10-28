92d0465368e3dbf3a176002db1d2c707ccfc9500
pogovind <pogovind@in.ibm.com>
2020-10-28 21:09:39 +0530

    updated the play books

--- roles/get-the-sap-version/files/sap-version-check.sh
+++ roles/get-the-sap-version/files/sap-version-check.sh
@@ -2,6 +2,6 @@
 sid=`db2 -x "select current server from sysibm.sysdummy1"`
 SCHEMA=`db2 -x "select tabschema from syscat.tables where tabname='CVERS_TXT'"`
 sapvers=`db2 -x "select distinct varchar(STEXT,25) from $SCHEMA.CVERS_TXT where LANGU='E'"`
-echo "$sid" > /tmp/sapvers.log
-echo "$sapvers" >> /tmp/sapvers.log
-disp+work|egrep -i 'variant|number' >> /tmp/sapvers.log
+echo "$sid" > /tmp/sap-version-check.log
+echo "'$sapvers'" >> /tmp/sap-version-check.log
+disp+work|egrep -i 'variant|number' >> /tmp/sap-version-check.log
