e37d581b948a321740f4be5dc0e3763476aa5aa5
pogovind <pogovind@in.ibm.com>
2020-11-02 13:45:54 +0530

    uploaded the logs

--- roles/get-the-sap-version-oradb/files/sap-version-check-oradb.sh
+++ roles/get-the-sap-version-oradb/files/sap-version-check-oradb.sh
@@ -1,8 +1,10 @@
 #!/bin/ksh
 sid=`env|grep -i DB_SID|cut -d"=" -f2`
+ssmall=`echo $sid|tr '[:upper:]' '[:lower:]'`
+sidadm="${ssmall}adm"
 schemaa=`env|grep -i dbs_ora_schema|cut -d"=" -f2`
 sqlplus -s sys/sys as sysdba<<EOF
-spool /tmp/sap-version-check-oradb.log;
+spool /tmp/$sidadm-sap-version-check-oradb.log;
 set echo off;
 set feedback off;
 set heading off;
@@ -11,11 +13,11 @@ select distinct STEXT from $schemaa.CVERS_TXT where LANGU='E';
 spool off;
 exit
 EOF
-sversion=`tail -1 /tmp/sap-version-check-oradb.log`
-DBVER=`cat /tmp/sap-version-check-oradb.log | grep -i version`
-DBREL=`cat /tmp/sap-version-check-oradb.log | grep -i oracle`
-echo "$sid" > /tmp/sap-version-check-oradb.log
-echo "$sversion" >> /tmp/sap-version-check-oradb.log
-disp+work|egrep -i 'variant|number' >> /tmp/sap-version-check-oradb.log
-echo "$DBREL" >> /tmp/sap-version-check-oradb.log
-echo "$DBVER" >> /tmp/sap-version-check-oradb.log
+sversion=`tail -1 /tmp/$sidadm-sap-version-check-oradb.log`
+DBVER=`cat /tmp/$sidadm-sap-version-check-oradb.log | grep -i version`
+DBREL=`cat /tmp/$sidadm-sap-version-check-oradb.log | grep -i oracle`
+echo "$sid" > /tmp/$sidadm-sap-version-check-oradb.log
+echo "$sversion" >> /tmp/$sidadm-sap-version-check-oradb.log
+disp+work|egrep -i 'variant|number' >> /tmp/$sidadm-sap-version-check-oradb.log
+echo "$DBREL" >> /tmp/$sidadm-sap-version-check-oradb.log
+echo "$DBVER" >> /tmp/$sidadm-sap-version-check-oradb.log
