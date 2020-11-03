89e3dcac36aeada37950f263af96260f523ce63f
pogovind <pogovind@in.ibm.com>
2020-11-03 22:38:18 +0530

    Uploaded files

--- roles/get-the-sap-version-db2-abap/files/sap-version-check-db2-abap.sh
+++ roles/get-the-sap-version-db2-abap/files/sap-version-check-db2-abap.sh
@@ -8,5 +8,5 @@ dblevel=`db2level | grep token | awk '{print $4 " " $5}' | sed 's/"//g' |  sed '
 kernellevel=`db2 -x  "select SAPRELEASE,Trim(Leading '0' from patchno),timestamp from $SCHEMA.PATCHHIST where EXECUTABLE='disp+work' order by 3" | awk '{print $1 " " $2}' | tail -1`
 echo "$sid" > /tmp/$sidadm-sap-version-check-db2-abap.log
 echo "$sapvers" >> /tmp/$sidadm-sap-version-check-db2-abap.log
-echo "$kernellevel" >> $sidadm-sap-version-check-db2-abap.log
-echo "$dblevel" >> $sidadm-sap-version-check-db2-abap.log
+echo "$kernellevel" >> /tmp/$sidadm-sap-version-check-db2-abap.log
+echo "$dblevel" >> /tmp/$sidadm-sap-version-check-db2-abap.log
