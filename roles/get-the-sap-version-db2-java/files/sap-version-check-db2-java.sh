e7247670b1c2d423dba52bd1bbece1dba7a35cf2
pogovind <pogovind@in.ibm.com>
2020-11-05 20:50:10 +0530

    Updated

--- roles/get-the-sap-version-db2-java/files/sap-version-check-db2-java.sh
+++ roles/get-the-sap-version-db2-java/files/sap-version-check-db2-java.sh
@@ -2,7 +2,7 @@
 sid=`db2 -x "select current server from sysibm.sysdummy1"`
 sidadm=`id -u -n`
 SCHEMA=`db2 -x "select tabschema from syscat.tables where tabname='BC_CVERS'"`
-sapvers=`db2 -x "select distinct SAPRELEASE,SERVICELEVEL from $SCHEMA.bc_compvers where scname='ENGINEAPI'"| egrep -v SAPRELEASE`
+sapvers=`db2 -x "select distinct substr(SAPRELEASE,1,3) as SAPRELEASE,SERVICELEVEL from SAPTKSDB.bc_compvers where scname='ENGINEAPI'"`
 dblevel=`db2level | grep token | awk '{print $4 " " $5}' | sed 's/"//g' |  sed 's/,//g'`
 echo "$sid" > /tmp/$sidadm-sap-version-check-db2-java.log
 echo "NW $sapvers" >> /tmp/$sidadm-sap-version-check-db2-java.log
