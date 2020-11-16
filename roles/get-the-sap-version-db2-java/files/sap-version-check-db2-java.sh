afe0bfa4d237d7fa2b51f4cd9fb193c16b03bb2d
pogovind <pogovind@in.ibm.com>
2020-11-17 00:23:33 +0530

    U

--- roles/get-the-sap-version-db2-java/files/sap-version-check-db2-java.sh
+++ roles/get-the-sap-version-db2-java/files/sap-version-check-db2-java.sh
@@ -2,9 +2,10 @@
 sid=`db2 -x "select current server from sysibm.sysdummy1"`
 sidadm=`id -u -n`
 SCHEMA=`db2 -x "select tabschema from syscat.tables where tabname='BC_CVERS'"`
-sapvers=`db2 -x "select distinct substr(SAPRELEASE,1,3) as SAPRELEASE,SERVICELEVEL from $SCHEMA.bc_compvers where scname='ENGINEAPI'"`
+Jrel=`db2 -x "select distinct substr(SAPRELEASE,1,3) as SAPRELEASE from $SCHEMA.bc_compvers where scname='ENGINEAPI'"`
+Jser=`db2 -x "select distinct SERVICELEVEL from $SCHEMA.bc_compvers where scname='ENGINEAPI'"`
 dblevel=`db2level | grep token | awk '{print $4 " " $5}' | sed 's/"//g' |  sed 's/,//g'`
 echo "$sid" > /tmp/$sidadm-sap-version-check-db2-java.log
-echo "NW $sapvers" >> /tmp/$sidadm-sap-version-check-db2-java.log
+echo "Java NW $Jrel SP $Jser" >> /tmp/$sidadm-sap-version-check-db2-java.log
 disp+work|egrep -i 'variant|number' >> /tmp/$sidadm-sap-version-check-db2-java.log
 echo "$dblevel" >> /tmp/$sidadm-sap-version-check-db2-java.log
