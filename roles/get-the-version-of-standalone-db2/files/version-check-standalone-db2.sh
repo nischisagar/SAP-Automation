174e7b30e88ab606fbfe675d08d6fb8fc11736ec
pogovind <pogovind@in.ibm.com>
2021-02-08 16:41:33 +0530

    Updated the code

--- roles/get-the-version-of-standalone-db2/files/version-check-standalone-db2.sh
+++ roles/get-the-version-of-standalone-db2/files/version-check-standalone-db2.sh
@@ -11,7 +11,10 @@ db2 connect to $dbname; db2 -x "SELECT service_level FROM TABLE  (sysproc.env_ge
 product=`cat /tmp/ansible-prd.txt`
 db2 connect to $dbname; db2 -x "SELECT service_level FROM TABLE  (sysproc.env_get_inst_info())" |  awk '{print $2}' > /tmp/ansible-ver.txt
 dbver=`cat /tmp/ansible-ver.txt`
+db2 connect to $dbname; db2 -x "SELECT round(SUM(db_size/1024/1024/1024),2) FROM systools.stmg_dbsize_info"| awk '{print $1}' > /tmp/ansible-size.txt
+dbsize=`cat /tmp/ansible-size.txt`
 echo "$dbname" > /tmp/ansible-$dbname-dbversion.log
 echo "$product" >> /tmp/ansible-$dbname-dbversion.log
 echo "$dbver" >> /tmp/ansible-$dbname-dbversion.log
+echo "$dbsize" >> /tmp/ansible-$dbname-dbversion.log
 done
