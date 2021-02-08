8c0021d0a6258ec4b8a3beb0c2d3ecd995b7eee1
pogovind <pogovind@in.ibm.com>
2021-02-08 19:50:47 +0530

    Updated the code

--- roles/get-the-version-of-standalone-oradb/files/version-check-standalone-oradb.sh
+++ roles/get-the-version-of-standalone-oradb/files/version-check-standalone-oradb.sh
@@ -16,12 +16,24 @@ spool /tmp/ansible-dbversion.txt;
 set heading off;
 SELECT version FROM V\$INSTANCE;
 spool off;
+spool /tmp/ansible-dbsize.txt;
+set heading off;
+select
+( select sum(bytes)/1024/1024/1024 data_size from dba_data_files ) +
+( select nvl(sum(bytes),0)/1024/1024/1024 temp_size from dba_temp_files ) +
+( select sum(bytes)/1024/1024/1024 redo_size from sys.v_\$log ) +
+( select sum(BLOCK_SIZE*FILE_SIZE_BLKS)/1024/1024/1024 controlfile_size from v\$controlfile) "Size in GB"
+from
+dual;
+spool off;
 exit
 EOF
 echo "fname = $fname"
 dbrel=`cat /tmp/ansible-dbrelease.txt | grep [a-z]`
 dbver=`cat /tmp/ansible-dbversion.txt|grep [0-9]`
+dbsize=`sed 's/ //g' /tmp/ansible-dbsize.txt|grep "^[0-9]"`
 echo "$db" > /tmp/ansible-$fname
 echo "$dbrel" >> /tmp/ansible-$fname
 echo "$dbver" >> /tmp/ansible-$fname
+echo "$dbsize" >> /tmp/ansible-$fname
 done
