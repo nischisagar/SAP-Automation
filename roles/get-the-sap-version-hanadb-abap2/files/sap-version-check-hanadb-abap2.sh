fb5b155921e9262492c8074fbd019e44e07fa14f
pogovind <pogovind@in.ibm.com>
2021-02-08 22:05:49 +0530

    Updated the code

--- roles/get-the-sap-version-hanadb-abap2/files/sap-version-check-hanadb-abap2.sh
+++ roles/get-the-sap-version-hanadb-abap2/files/sap-version-check-hanadb-abap2.sh
@@ -4,8 +4,10 @@ sidadm=`id -u -n`
 SCHEMA=`hdbsql -U hcuser2  -j -x -C "select schema_name from tables where table_name='USR02'" | egrep -vi schema_name`
 sapvers=`hdbsql -U hcuser2  -j -x -C "select distinct STEXT from $SCHEMA.CVERS_TXT where LANGU='E'" | egrep -v STEXT`
 kernellevel=`hdbsql -U hcuser2  -j -x -C "select SAPRELEASE,Trim(Leading '0' from patchno),timestamp from $SCHEMA.PATCHHIST where EXECUTABLE='disp+work' order by 3" | sed 's/,/ /g'| awk '{print $1 " " $2}' | tail -1`
+dbsize=`hdbsql -U hcuser2  -j -x -C "SELECT ROUND(TO_DOUBLE(sum(disk_size)/1024/1024/1024),2) "DB_SIZE" FROM m_table_persistence_statistics" | tail -1`
 hdblevel=`HDB -version|grep -i version|tail -1|awk '{print $2}'|cut -d"." -f1-3`
 echo "$sid" > /tmp/$sidadm-sap-version-check-hanadb-abap2.log
 echo "$sapvers" >> /tmp/$sidadm-sap-version-check-hanadb-abap2.log
 echo "$kernellevel" >>  /tmp/$sidadm-sap-version-check-hanadb-abap2.log
 echo "HANA $hdblevel" >>  /tmp/$sidadm-sap-version-check-hanadb-abap2.log
+echo "$dbsize" >> /tmp/$sidadm-sap-version-check-hanadb-abap2.log
