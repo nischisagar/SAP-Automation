0c1aba3f0536c9cb6dbe96d276ff1003e356d454
pogovind <pogovind@in.ibm.com>
2021-02-08 17:16:06 +0530

    Updated the code

--- roles/get-the-sap-version-oradb-abap/files/sap-version-check-oradb-abap.sh
+++ roles/get-the-sap-version-oradb-abap/files/sap-version-check-oradb-abap.sh
@@ -24,14 +24,27 @@ set pagesize 500;
 spool /tmp/$sidadm-ansible_kernelv.log;
 select SAPRELEASE,Trim(Leading '0' from patchno),timestamp from $schemaa.PATCHHIST where EXECUTABLE='disp+work' order by 3 desc fetch  first 1 rows only;
 spool off;
+set linesize 500;
+set pagesize 500;
+spool /tmp/$sidadm-ansible-dbsize.log;
+select
+( select sum(bytes)/1024/1024/1024 data_size from dba_data_files ) +
+( select nvl(sum(bytes),0)/1024/1024/1024 temp_size from dba_temp_files ) +
+( select sum(bytes)/1024/1024/1024 redo_size from sys.v_\$log ) +
+( select sum(BLOCK_SIZE*FILE_SIZE_BLKS)/1024/1024/1024 controlfile_size from v\$controlfile) "Size in GB"
+from
+dual;
+spool off;
 exit
 EOF
 sversion=`tail -1 /tmp/$sidadm-ansible_sapvers.log`
 DBREL=`cat /tmp/$sidadm-ansible_sapvers.log|grep -i oracle`
 DBVER=`cat /tmp/$sidadm-ansible_sapvers.log|grep -i version`
 kernellevel=`cat /tmp/$sidadm-ansible_kernelv.log|tail -1|awk '{print $1,$2}'`
+dbsize=`sed 's/ //g' /tmp/$sidadm-ansible-dbsize.log|grep "^[0-9]"`
 echo "$sid" > /tmp/$sidadm-sap-version-check-oradb-abap.log
 echo "$sversion" >> /tmp/$sidadm-sap-version-check-oradb-abap.log
 echo "$kernellevel" >> /tmp/$sidadm-sap-version-check-oradb-abap.log
 echo "$DBREL" >> /tmp/$sidadm-sap-version-check-oradb-abap.log
 echo "$DBVER" >> /tmp/$sidadm-sap-version-check-oradb-abap.log
+echo "$dbsize" >> /tmp/$sidadm-sap-version-check-oradb-abap.log
