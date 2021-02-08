8bb76bdb0f0db4b2dad5f6be4b63d66b3713eb80
pogovind <pogovind@in.ibm.com>
2021-02-08 13:44:35 +0530

    Updated the code

--- roles/get-the-sap-version-db2-abap/files/sap-version-check-db2-abap.sh
+++ roles/get-the-sap-version-db2-abap/files/sap-version-check-db2-abap.sh
@@ -5,7 +5,9 @@ SCHEMA=`db2 -x "select tabschema from syscat.tables where tabname='CVERS_TXT'"`
 sapvers=`db2 -x "select distinct varchar(STEXT,25) from $SCHEMA.CVERS_TXT where LANGU='E'"`
 dblevel=`db2level | grep token | awk '{print $4 " " $5}' | sed 's/"//g' |  sed 's/,//g'`
 kernellevel=`db2 -x  "select SAPRELEASE,Trim(Leading '0' from patchno),timestamp from $SCHEMA.PATCHHIST where EXECUTABLE='disp+work' order by 3" | awk '{print $1 " " $2}' | tail -1`
+dbsize=`db2 -x "SELECT round(SUM(db_size/1024/1024/1024),2) FROM systools.stmg_dbsize_info"| awk '{print $1}'`
 echo "$sid" > /tmp/$sidadm-sap-version-check-db2-abap.log
 echo "$sapvers" >> /tmp/$sidadm-sap-version-check-db2-abap.log
 echo "$kernellevel" >> /tmp/$sidadm-sap-version-check-db2-abap.log
 echo "$dblevel" >> /tmp/$sidadm-sap-version-check-db2-abap.log
+echo "$dbsize" >> /tmp/$sidadm-sap-version-check-db2-abap.log
