984ea60460a23544aae5168f92483d965ace9e41
sureshkk <sureshkk@in.ibm.com>
2021-02-22 21:39:19 +0530

    sap sql query update

--- roles/get-the-sap-version-oradb-win-java/files/ora_sap.sql
+++ roles/get-the-sap-version-oradb-win-java/files/ora_sap.sql
@@ -1,5 +1,5 @@
 set linesize 500
 set pagesize 500
 set heading off
-select distinct STEXT from SAPSR3.CVERS_TXT where LANGU='E';
+select distinct SAPRELEASE from SAPSR3DB.BC_COMPVERS where SCNAME='CORE-TOOLS';
 exit
