69e3edce4104fcac103335582c5306798c045908
sivanamu@in.ibm.com <sivanamu@in.ibm.com>
2021-07-01 19:17:06 +0530

    SAP Inventory Data Collection for App Server

--- roles/get-the-sap-version-oradb-abap-with-appserver/files/sap-version-check-oradb-abap-with-app.sh
+++ roles/get-the-sap-version-oradb-abap-with-appserver/files/sap-version-check-oradb-abap-with-app.sh
@@ -1,6 +1,8 @@
 #!/bin/ksh
 sid=`env|grep -i DB_SID|cut -d"=" -f2`
 sidadm=`id -u -n`
+sidsmall=`echo $sid|tr '[:upper:]' '[:lower:]'`
+sapsidadm="${sidsmall}adm"
 sqlplus -s sys/sys as sysdba<<EOF
 spool /tmp/$sidadm-ansible_schemanamee.txt;
 set echo off;
@@ -26,7 +28,7 @@ select SAPRELEASE,Trim(Leading '0' from patchno),timestamp from $schemaa.PATCHHI
 spool off;
 set linesize 500;
 set pagesize 500;
-spool /tmp/$sidadm-ansible-dbsize.log;
+spool /tmp/$sidadm-dbsize_new.log;
 select
 ( select sum(bytes)/1024/1024/1024 data_size from dba_data_files ) +
 ( select nvl(sum(bytes),0)/1024/1024/1024 temp_size from dba_temp_files ) +
@@ -41,10 +43,19 @@ sversion=`tail -1 /tmp/$sidadm-ansible_sapvers.log`
 DBREL=`cat /tmp/$sidadm-ansible_sapvers.log|grep -i oracle`
 DBVER=`cat /tmp/$sidadm-ansible_sapvers.log|grep -i version`
 kernellevel=`cat /tmp/$sidadm-ansible_kernelv.log|tail -1|awk '{print $1,$2}'`
-dbsize=`sed 's/ //g' /tmp/$sidadm-ansible-dbsize.log|grep "^[0-9]"`
+dbsize=`sed 's/ //g' /tmp/$sidadm-dbsize_new.log|grep "^[0-9]"`
 echo "$sid" > /tmp/$sidadm-sap-version-check-oradb-abap.log
+echo "$sid" > /sapmnt/$sid/sapversion/$sapsidadm-sapapp-version-check-abap.log
 echo "$sversion" >> /tmp/$sidadm-sap-version-check-oradb-abap.log
+echo "$sversion" >> /sapmnt/$sid/sapversion/$sapsidadm-sapapp-version-check-abap.log
 echo "$kernellevel" >> /tmp/$sidadm-sap-version-check-oradb-abap.log
+echo "$kernellevel" >> /sapmnt/$sid/sapversion/$sapsidadm-sapapp-version-check-abap.log
 echo "$DBREL" >> /tmp/$sidadm-sap-version-check-oradb-abap.log
+echo "NA" >> /sapmnt/$sid/sapversion/$sapsidadm-sapapp-version-check-abap.log
 echo "$DBVER" >> /tmp/$sidadm-sap-version-check-oradb-abap.log
+echo "NA" >> /sapmnt/$sid/sapversion/$sapsidadm-sapapp-version-check-abap.log
 echo "$dbsize" >> /tmp/$sidadm-sap-version-check-oradb-abap.log
+echo "NA" >> /sapmnt/$sid/sapversion/$sapsidadm-sapapp-version-check-abap.log
+echo "ABAP" >> /tmp/$sidadm-sap-version-check-oradb-abap.log
+echo "ABAP" >> /sapmnt/$sid/sapversion/$sapsidadm-sapapp-version-check-abap.log
+chmod 775 /sapmnt/$sid/sapversion/$sapsidadm-sapapp-version-check-abap.log
