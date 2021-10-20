894c3a843b763ae2d8fa9b7bf46592c0b952d5c3
kalaikannan <Kalaikannan.Anbarasan@ocean.ibm.com>
2021-10-20 12:24:18 +0530

    updated

---
+++ roles/Oracle_standalone_DB_creation/templates/Oracle_DBcreation_script.sh
@@ -0,0 +1,10 @@
+export ORACLE_SID={{ dbname }}
+export ORACLE_HOME={{ oracle_home_value }}
+export ORACLE_BASE={{ oracle_base_value }}
+export PATH={{ oracle_home_value }}/bin:$PATH
+sqlplus -s sys/sys as sysdba<<EOF
+startup nomount pfile='/oracle/stage/init_test.ora'
+spool /oracle/stage/dbcreatestatus.txt
+spool off
+exit
+EOF
