29b1263d382646b108c661d4344bc55022abe9e5
kalaikannan <Kalaikannan.Anbarasan@ocean.ibm.com>
2022-05-18 18:36:10 +0530

    updated

--- roles/get-the-sap-version-maxdb-ads-java/files/sap-maxdb-version-check.sh
+++ roles/get-the-sap-version-maxdb-ads-java/files/sap-maxdb-version-check.sh
@@ -1,5 +1,5 @@
 #!/bin/ksh
-sid=`env|grep -i SAPSYSTEMNAME|cut -d"=" -f2`
+sid=`df -g|grep -i sapdb|grep -i saplog|grep -v logbackup|awk '{print $NF}'|cut -d/ -f3`
 dbmcli -d $sid -U w <<EOF
 db_enum
 exit
