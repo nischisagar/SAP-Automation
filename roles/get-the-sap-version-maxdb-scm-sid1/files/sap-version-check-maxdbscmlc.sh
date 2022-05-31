10c3775084aabd0df5e32987dc90c6658fafb02f
kalaikannan <Kalaikannan.Anbarasan@ocean.ibm.com>
2022-05-31 12:00:08 +0530

    updated

--- roles/get-the-sap-version-maxdb-scm-sid1/files/sap-version-check-maxdbscmlc.sh
+++ roles/get-the-sap-version-maxdb-scm-sid1/files/sap-version-check-maxdbscmlc.sh
@@ -2,4 +2,5 @@
 sidadm=`id -u -n`
 sid1=`df -g|grep -i sapdb|grep -i saplog|grep -v logbackup|grep -v saplogbck|awk '{print $NF}'|cut -d/ -f3`
 echo $sid1 > /tmp/$sidadm-sapscm-lc-version.log
+echo "MAXDB" >> /tmp/$sidadm-sapscm-lc-version.log
 dbmcli -d $sid1 -c "db_enum"|grep -i running|awk '{print $3}' >> /tmp/$sidadm-sapscm-lc-version.log
