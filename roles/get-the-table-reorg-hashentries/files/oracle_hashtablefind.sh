31d570cdc2bba90ef74c1ae5f6c92aab59dc8e8b
kalaikannan <Kalaikannan.Anbarasan@ocean.ibm.com>
2022-06-14 13:03:39 +0530

    updated

---
+++ roles/get-the-table-reorg-hashentries/files/oracle_hashtablefind.sh
@@ -0,0 +1,13 @@
+#!/bin/ksh
+sidadm=`id -u -n`
+hname=`hostname`
+sqlplus -s sys/sys as sysdba<<EOF
+Set heading off
+spool /tmp/$sidadm-hashtable.log;
+select segment_name,segment_type,bytes/1024/1024 as "size in MB" from dba_segments where segment_name like '%#$';
+select instance_name,HOST_NAME from v\$instance;
+spool off;
+exit
+EOF
+cat /tmp/$sidadm-hashtable.log|sed '/^$/d' >> /tmp/$hname-hashtable_final.log
+echo "=====================================================================================================" >> /tmp/$hname-hashtable_final.log
