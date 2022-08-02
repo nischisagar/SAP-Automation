305c54962afb0d978aa6009b172c8367054e723e
kalaikannan <Kalaikannan.Anbarasan@ocean.ibm.com>
2022-08-02 15:58:20 +0530

    upd

---
+++ roles/itm_agent_monitoring_status_multitenant23/files/sap-version-check-hanadb-abap2.sh
@@ -0,0 +1,13 @@
+#!/bin/ksh
+sid=`hdbsql -U hcuser2  -j -x -C "select database_name from m_databases" | egrep -vi database_name`
+sidadm=`id -u -n`
+SCHEMA=`hdbsql -U hcuser2  -j -x -C "select schema_name from tables where table_name='USR02'" | egrep -vi schema_name`
+sapvers=`hdbsql -U hcuser2  -j -x -C "select distinct STEXT from $SCHEMA.CVERS_TXT where LANGU='E'" | egrep -v STEXT`
+kernellevel=`hdbsql -U hcuser2  -j -x -C "select SAPRELEASE,Trim(Leading '0' from patchno),timestamp from $SCHEMA.PATCHHIST where EXECUTABLE='disp+work' order by 3" | sed 's/,/ /g'| awk '{print $1 " " $2}' | tail -1`
+dbsize=`hdbsql -U hcuser2  -j -x -C "SELECT ROUND(TO_DOUBLE(sum(disk_size)/1024/1024/1024),2) "DB_SIZE" FROM m_table_persistence_statistics" | tail -1`
+hdblevel=`HDB -version|grep -i version|tail -1|awk '{print $2}'|cut -d"." -f1-3`
+echo "$sid" > /tmp/$sidadm-sap-version-check-hanadb-abap2.log
+echo "$sapvers" >> /tmp/$sidadm-sap-version-check-hanadb-abap2.log
+echo "$kernellevel" >>  /tmp/$sidadm-sap-version-check-hanadb-abap2.log
+echo "HANA $hdblevel" >>  /tmp/$sidadm-sap-version-check-hanadb-abap2.log
+echo "$dbsize" >> /tmp/$sidadm-sap-version-check-hanadb-abap2.log
