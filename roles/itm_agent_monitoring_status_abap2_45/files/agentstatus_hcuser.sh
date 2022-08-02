9bfda06e1b764b4ca15328cebead7baa048fc2ed
kalaikannan <Kalaikannan.Anbarasan@ocean.ibm.com>
2022-08-02 16:26:18 +0530

    updated

---
+++ roles/itm_agent_monitoring_status_abap2_45/files/agentstatus_hcuser.sh
@@ -0,0 +1,6 @@
+#!/bin/ksh
+sid=`hdbsql -U hcuser -j -x -C "select database_name from m_databases" | egrep -vi database_name`
+SCHEMA=`hdbsql -U hcuser -j -x -C "select schema_name from tables where table_name='USR02'" | egrep -vi schema_name`
+agstatus=`hdbsql -U hcuser -j -x -C "select UFLAG from $SCHEMA.usr02 where BNAME='IBMMON_AGENT' and MANDT='000'"|tail -1`
+echo $sid > /tmp/itmuserstatus_hanadb_$sid
+echo $agstatus >> /tmp/itmuserstatus_hanadb_$sid
