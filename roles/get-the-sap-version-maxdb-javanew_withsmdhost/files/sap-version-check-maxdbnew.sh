952815cbba965180cd9974dc67f62618e7e06a43
kalaikannan <Kalaikannan.Anbarasan@ocean.ibm.com>
2023-01-12 15:21:38 +0530

    updated

---
+++ roles/get-the-sap-version-maxdb-javanew_withsmdhost/files/sap-version-check-maxdbnew.sh
@@ -0,0 +1,11 @@
+#!/bin/ksh
+sid=`env|grep -i SAPSYSTEMNAME|cut -d"=" -f2`
+sidadm=`id -u -n`
+batpath=`find /usr/sap/$sid/*/j2ee/configtool -name batchconfig.csh`
+adssapv=`$batpath -task get.versions.of.deployed.units|grep -i adssap|head -1`
+adsv=`echo $adssapv|cut -d: -f2|cut -d. -f2-3`
+adsp=`echo $adssapv|cut -d: -f2|cut -d. -f4`
+echo $sid > /tmp/$sidadm-sapads-maxdb-version.log
+echo SAP NetWeaver $adsv SP $adsp >> /tmp/$sidadm-sapads-maxdb-version.log
+disp+work|egrep -i 'variant|number' >> /tmp/$sidadm-sapads-maxdb-version.log
+echo "MaxDB" >> /tmp/$sidadm-sapads-maxdb-version.log
