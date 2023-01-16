d7e34f73221d0534744f25fa439fb049a14ac702
kalaikannan <Kalaikannan.Anbarasan@ocean.ibm.com>
2023-01-16 19:47:45 +0530

    updated

---
+++ roles/get-sap-db2hana-app-version_withsmdhost/files/get-sap-db2hana-app-version_withsmdhost.sh
@@ -0,0 +1,27 @@
+#!/bin/ksh
+rm -f /ansible/reports/outputfinal.csv
+tgtlist=`cat /ansible/reports/db2-hana-app-servers-list|awk '{print $1}'|uniq`
+ddate=`date +%Y-%m-%d`
+csvfile=`ls -lrt /ansible/reports/unix-linux-sap-db-os-smd-host-versions.csv|awk '{print $9}'`
+echo $csvfile
+echo $tgtlist
+for i in $tgtlist
+do
+echo $i
+srcsid=`cat $csvfile|grep -v 012_SEKURIT|grep -v xmpsa013pbw0|grep -i $i`
+echo $srcsid
+echo $srcsid > /ansible/reports/temp1.csv
+srchname=`cat /ansible/reports/temp1.csv|cut -d, -f2`
+echo $srchname
+updlist=`cat /ansible/reports/db2-hana-app-servers-list|grep -i $i|awk '{print $3}'`
+echo $updlist
+for j in $updlist
+do
+echo $srchname
+echo $j
+value=`sed "s/$srchname/$j/g" /ansible/reports/temp1.csv`
+echo $value
+echo $value >> /ansible/reports/outputfinal.csv
+done
+done
+cat /ansible/reports/outputfinal.csv >> $csvfile
