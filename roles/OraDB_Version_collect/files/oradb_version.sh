07548887902114d59fc423675359324f3e8cd1d3
SatishAbraham <satish.abraham@ocean.ibm.com>
2023-06-21 17:55:18 +0530

    format change

--- roles/OraDB_Version_collect/files/oradb_version.sh
+++ roles/OraDB_Version_collect/files/oradb_version.sh
@@ -4,6 +4,9 @@ sqlplus -s sys/sys as sysdba<<EOF
 set feedback off
 set heading off
 set echo off
+set line 300
+set pages 400
+col host_name for a25
 spool /tmp/ora_version_$idm.txt
 select host_name,instance_name,VERSION_FULL from v\$instance;
 spool off;
